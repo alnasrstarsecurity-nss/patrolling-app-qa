053f64a2ea1ed106540fc210efd4bd570699ccf5
alnasrstarsecurity-nss <alnasrstarit@gmail.com>
2026-01-10 13:16:29 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwLiweO0ftYVNVXDHJIFiSzBOhAAp78_wwT2tuPcEnprUrV-mdFalDMi6-lF6tqM7Ib/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwdADvhTO2OiJC2FFD97gF2MOvIiWF2i0xZsp2C0Hkk4DEbmY5xb87h_kEqzpytzeNVAQ/exec";
 
 function requireLogin() {
   if (!localStorage.getItem("user") || !localStorage.getItem("pass")) {
